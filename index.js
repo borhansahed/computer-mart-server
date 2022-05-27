bd3cf25b4741478a0a3cc5887db8519b9f1daabd
wahi90 <mdshahed1098@gmail.com>
2022-05-27 16:14:13 +0600

    doing booking payment route

--- index.js
+++ index.js
@@ -98,6 +98,13 @@ async function run(){
 
       })
 
+      app.get('/booking/:id' , verifyJWT , async (req, res ) =>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const booking = await bookingCollection.findOne(query);
+        res.send(booking); 
+      })
+
       app.get('/admin/:email' , async(req, res )=>{
        
        const email = req.params.email;
