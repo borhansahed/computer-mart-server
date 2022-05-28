f70a32db8bc73bb99811210984fbd8f5b49ab988
wahi90 <mdshahed1098@gmail.com>
2022-05-28 19:27:17 +0600

    test

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ async function run(){
 
         app.get('/product', async(req,res) =>{
          const query = {};
-        const cursor = productCollection.find(query);
+        const cursor = productCollection.find(query).sort({ $natural: -1 }).limit(N);
         const products = await cursor.toArray();
         res.send(products);
         });
